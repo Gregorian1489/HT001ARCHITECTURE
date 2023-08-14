fb6f533f7eeaf42ff8f7889366471c4331229df4
Gregorian1489 <santijago@mail.ru>
2023-08-14 14:22:59 +0400

    add HomeTask001 AR v.2

--- ModelElements/Scene.java
+++ ModelElements/Scene.java
@@ -1,3 +1,4 @@
+import java.io.EOFException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,8 +11,16 @@ public class Scene {
 
     public Scene(int id, List<PoligonalModel> models, List<Flash> flashes, List<Camera> cameras) throws Exception {
         this.ID = id;
+
+        if (models.size() > 0){
         this.Models = models;
+        } else throw new Exception("Должна быть хотя бы одна модель");
+
+        this.Flashes = flashes;
+
+        if (cameras.size() > 0){
         this.cameras = cameras;
+        } else throw new Exception("Должны быть хотя бы одна камера");
     }
     public <T> T method1(T flash) {
         return flash;
